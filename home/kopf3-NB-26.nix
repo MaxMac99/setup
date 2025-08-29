d0cd1edec54a8b2d1214197f30cd9e5848cfc6e4
Max Vissing <max_vissing@yahoo.de>
2025-08-29 13:17:54 +0200

    Add kopf3-NB-26

---
+++ home/kopf3-NB-26.nix
@@ -0,0 +1,11 @@
+{...}: {
+  imports = [
+    common/core
+
+    common/optional/browsers/chrome.nix
+
+    common/optional/terminals/ghostty.nix
+    common/optional/ides/intellij.nix
+    common/optional/ides/rust-rover.nix
+  ];
+}
---
+++ hosts/darwin/kopf3-NB-26/default.nix
@@ -0,0 +1,21 @@
+{lib, ...}: {
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  imports = lib.flatten [
+    (map lib.custom.relativeToRoot [
+      "hosts/common/core"
+    ])
+  ];
+
+  homebrew.casks = [
+  ];
+
+  hostSpec = {
+    username = "maxvissing";
+    hostName = "kopf3-NB-26";
+    isDarwin = true;
+    isWork = true;
+    isServer = false;
+    isMinimal = false;
+  };
+}
