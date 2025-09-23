0b249bef4471780a1eb94c8acf4501b52b1c8cd9
Max Vissing <max_vissing@yahoo.de>
2025-09-23 16:34:08 +0200

    Add rust

--- home/common/core/default.nix
+++ home/common/core/default.nix
@@ -46,7 +46,6 @@
       fzf
       eza
       cargo
-      rustc
       nixpkgs-fmt
       selene
       statix
--- home/common/optional/ides/rust-rover.nix
+++ home/common/optional/ides/rust-rover.nix
@@ -1,5 +1,22 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  rustToolchain = pkgs.symlinkJoin {
+    name = "rust-toolchain";
+    paths = [
+      pkgs.rustc
+      pkgs.cargo
+      pkgs.rustfmt
+      pkgs.clippy
+    ];
+    postBuild = ''
+      mkdir -p $out/lib/rustlib/src
+      ln -s ${pkgs.rustPlatform.rustcSrc} $out/lib/rustlib/src/rust
+    '';
+  };
+in {
+  home.file.".rust-toolchain".source = rustToolchain;
   home.packages = with pkgs; [
     jetbrains.rust-rover
+    rustToolchain
+    rust-analyzer
   ];
 }
