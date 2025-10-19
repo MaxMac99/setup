731f2ea86b11768e68d65fbe2d3f8b17345b8de7
Max Vissing <max_vissing@yahoo.de>
2025-10-19 10:59:07 +0200

    Use microvms instead of pulumi for managing k3s vms

--- hosts/nixos/k3s-node2/default.nix
+++ hosts/nixos/k3s-node2/default.nix
@@ -1,7 +1,6 @@
-{ lib, ... }:
-
 let
-  template = import (lib.custom.relativeToRoot "modules/nixos/k3s-node-template.nix") { inherit lib; };
+  lib = (import <nixpkgs> {}).lib;
+  template = import ../../modules/nixos/k3s-node-template.nix { inherit lib; };
 in
 template.mkK3sNode {
   nodeName = "k3s-node2";
