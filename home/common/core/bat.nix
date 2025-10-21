dcb9d7c8c58dc47b73873342f9edb1536a8a7333
Max Vissing <max_vissing@yahoo.de>
2025-10-21 23:21:37 +0200

    Fix update

--- home/common/core/bat.nix
+++ home/common/core/bat.nix
@@ -1,8 +1,15 @@
 # https://github.com/sharkdp/bat
 # https://github.com/eth-p/bat-extras
 {pkgs, ...}: let
-  # Override bat-extras to skip tests due to snapshot mismatches
-  bat-extras-no-tests = pkgs.bat-extras.overrideAttrs (oldAttrs: {
+  # Override individual bat-extras packages to skip tests due to snapshot mismatches
+  # These tests fail because bat 0.25.0 changed its output format
+  batgrep-no-tests = pkgs.bat-extras.batgrep.overrideAttrs (oldAttrs: {
+    doCheck = false;
+  });
+  batdiff-no-tests = pkgs.bat-extras.batdiff.overrideAttrs (oldAttrs: {
+    doCheck = false;
+  });
+  batman-no-tests = pkgs.bat-extras.batman.overrideAttrs (oldAttrs: {
     doCheck = false;
   });
 in {
@@ -14,14 +21,11 @@ in {
       theme = "tokyonight";
       paging = "never";
     };
-    extraPackages = builtins.attrValues {
-      inherit
-        (bat-extras-no-tests)
-        batgrep # search through and highlight files using ripgrep
-        batdiff # Diff a file against the current git index, or display the diff between to files
-        batman # read manpages using bat as the formatter
-        ;
-    };
+    extraPackages = [
+      batgrep-no-tests # search through and highlight files using ripgrep
+      batdiff-no-tests # Diff a file against the current git index, or display the diff between to files
+      batman-no-tests # read manpages using bat as the formatter
+    ];
     themes = {
       tokyonight = {
         src = pkgs.fetchFromGitHub {
