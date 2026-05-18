ea72ffcb409f8b7a48ec1894cc17888e2870123b
Max Vissing <max_vissing@yahoo.de>
2026-05-18 09:48:35 +0200

    Fix tailscale

--- modules/apps/tailscale.nix
+++ modules/apps/tailscale.nix
@@ -1,12 +1,11 @@
 # Tailscale - nixos service on linux, homebrew cask on darwin
+# NixOS hosts that need peer-to-peer connectivity should also set
+# services.tailscale.openFirewall = true; in their host config.
 {
   pkgs,
   lib,
   ...
 }: {
-  services.tailscale = lib.mkIf pkgs.stdenv.isLinux {
-    enable = true;
-    openFirewall = true;
-  };
+  services.tailscale.enable = lib.mkIf pkgs.stdenv.isLinux true;
   homebrew.casks = lib.mkIf pkgs.stdenv.isDarwin ["tailscale-app"];
 }
