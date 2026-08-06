90d0ac8132b084cf271d9a619556098f4becd2c7
Max Vissing <max_vissing@yahoo.de>
2026-08-06 08:23:26 +0200

    Change directory prompt

--- modules/profiles/core-user/zsh.nix
+++ modules/profiles/core-user/zsh.nix
@@ -32,6 +32,12 @@
       #      bindkey '^[[Z' backward-word      # shift+tab
       #      bindkey '^ ' autosuggest-accept   # ctrl+space
 
+      # robbyrussell shows only the leaf directory (%c), which is useless with the
+      # worktree layout where every leaf is just the branch name. Show up to the
+      # last 3 components instead, eliding the rest.
+      PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) %{$fg[cyan]%}%(4~|…/%3~|%~)%{$reset_color%}"
+      PROMPT+=' $(git_prompt_info)'
+
       # K3s cluster kubeconfig
       export KUBECONFIG=~/.kube/k3s-config
 
