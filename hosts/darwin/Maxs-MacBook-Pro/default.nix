0654de39937e80c7ba7dc329511a13835df6e3c8
Max Vissing <max_vissing@yahoo.de>
2026-08-07 10:54:35 +0200

    feat(kubeconfig): roll out cluster access declaratively

--- hosts/darwin/Maxs-MacBook-Pro/default.nix
+++ hosts/darwin/Maxs-MacBook-Pro/default.nix
@@ -35,6 +35,7 @@
     "modules/apps/docker-desktop.nix"
     "modules/apps/insomnia.nix"
     "modules/apps/k9s.nix"
+    "modules/apps/kubeconfig.nix"
     "modules/apps/opencode"
     "modules/apps/1password.nix"
     "modules/apps/teleport.nix"
